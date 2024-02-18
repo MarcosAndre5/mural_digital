e1e10d42b1b00a3afe066de024faeb2e7b53744a
marcos <marcos.andre@funpec.br>
2024-02-18 20:28:17 -0300

    Limpeza no projeto de mural digital.

--- app/Http/Controllers/MuralController.php
+++ app/Http/Controllers/MuralController.php
@@ -11,47 +11,33 @@ class MuralController extends Controller {
     public function index() { return view('mural.index'); }
 
     public function create() {
-        //$mural = Mural::findOrFail(1);
-        
-        $mural = new Mural;
-        $mural->link = 'https://www.youtube.com/watch?v=vKw8LptzE54&list=PLFrJDLhPtm1HDv0IU3yxpGqfOQlv7lzKS&ab_channel=FUNPECRN';
-        $mural->video = 'vid_zxeGMiLX.mp4';
-        $mural->renderizar = 'link';
-        $mural->informativo1 = 'Estamos no topo da montanha, não temos nada disso ao nosso dispor, o que fazemos?A inspiração é mais forte que nós e toma o controlo, assim como os 300 caracteres tomam o controlo deste texto, que andando às voltas, não sabe se quer chegar a algum lado.Retomamos o fôlego, a inspiração é um apelo fa.';
-        $mural->informativo2 = 'Teste Funpec 2';
-        $mural->informativo3 = 'Teste Funpec 3';
+        $mural = Mural::findOrFail(1);
 
         return view('mural.create', compact('mural'));
     }
 
     public function store(Request $request) {
-        //$mural = Mural::findOrFail(1);
+        $mural = Mural::findOrFail(1);
         
-        //$mural->link = $request->get('link');
-        //$mural->informativo = $request->get('informativo');
+        $mural->link = $request->get('link');
+        $mural->renderizar = $request->get('renderizar');
+        $mural->informativo1 = $request->get('informativo1');
+        $mural->informativo2 = $request->get('informativo2');
+        $mural->informativo3 = $request->get('informativo3');
 
         if($request->hasFile('video')) {
             unlink(public_path('img/video/'.$mural->video));
 			$imagem = Input::file('video');
             $imagem->move(public_path('img/video/'), $imagem->getClientOriginalName());
-            //$mural->video = $imagem->getClientOriginalName();
+            $mural->video = $imagem->getClientOriginalName();
 		}
-
-        //$mural->update();
+        $mural->update();
 
         return Redirect::back()->with('message', 'Mural Atualizado com Sucesso!');
     }
 
     public function show() {
-        //$mural = Mural::findOrFail(1);
-
-        $mural = new Mural;
-        $mural->link = 'https://www.youtube.com/watch?v=vKw8LptzE54&list=PLFrJDLhPtm1HDv0IU3yxpGqfOQlv7lzKS&ab_channel=FUNPECRN';
-        $mural->video = 'vid_zxeGMiLX.mp4';
-        $mural->renderizar = 'link';
-        $mural->informativo1 = 'Estamos no topo da montanha, não temos nada disso ao nosso dispor, o que fazemos?A inspiração é mais forte que nós e toma o controlo, assim como os 300 caracteres tomam o controlo deste texto, que andando às voltas, não sabe se quer chegar a algum lado.Retomamos o fôlego, a inspiração é um apelo fa.';
-        $mural->informativo2 = 'Teste Funpec 2';
-        $mural->informativo3 = 'Teste Funpec 3';
+        $mural = Mural::findOrFail(1);
 
         if($mural->link != '') {
             $link = $mural->link;
--- app/Mural.php
+++ app/Mural.php
@@ -5,15 +5,15 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Mural extends Model {
-    //protected $table = 'mural';
+    protected $table = 'mural';
 
-    //protected $primaryKey = 'id';
+    protected $primaryKey = 'id';
 
-	//public $timestamps = false;
+	public $timestamps = false;
 
 	protected $fillable = [
 		'link', 'video', 'renderizar', 'informativo1', 'informativo2', 'informativo3'
 	];
 
-	//protected $guarded = [];
+	protected $guarded = [];
 }
